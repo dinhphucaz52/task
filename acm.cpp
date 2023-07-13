e5f467d2ffcbc3b5e90191b03ea46528571d5bfd
DP <dinh.phuc.17.5.25@gmail.com>
2023-07-13 21:16:20 +0700

    changes 	modified:   acm.cpp 	modified:   array_division.cpp 	modified:   baba.cpp 	modified:   bai_tap.cpp

--- acm.cpp
+++ acm.cpp
@@ -53,7 +53,7 @@ void solve(){
 }
 
 
-int32_t main(){
+signed main(){
 
     ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     /*-------------*/
--- baba.cpp
+++ baba.cpp
@@ -71,7 +71,7 @@ void solve(){
         cout<<s[i];
 }
 
-int32_t main(){
+signed main(){
 
     ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     /*-------------*/
